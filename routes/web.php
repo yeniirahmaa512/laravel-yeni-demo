25266efef41be6d2c82f25ba4f6f4492ae0f4f88
unknown <yenirahma396@gmail.com>
2025-01-21 16:22:41 +0700

    vid8

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
 });
+ 
+
+Route::resource("products", ProductController::class);
